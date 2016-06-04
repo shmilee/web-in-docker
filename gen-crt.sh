aeb3b6cd60244bea5fff7f63a06c85ed303b0fff
shmilee <shmilee.zju@gmail.com>
2016-06-04 10:35:39 +0800

    gen CA.crt

--- gen-crt.sh
+++ gen-crt.sh
@@ -1,39 +1,42 @@
 #!/bin/bash
 
+CA_dir=./CA
+CA_cnf=$CA_dir/openssl.cnf
+
 gen_CA () {
     #CA 证书请求生成及签名
-    openssl req -new -x509 -days 3650 -keyout ca.key -out ca.crt
+    openssl req -new -x509 -days 3650 -config $CA_cnf -keyout $CA_dir/CA.key -out $CA_dir/CA.crt
+    rm -rf $CA_dir/newcerts $CA_dir/index.* $CA_dir/serial*
 }
 
 gen_server () {
     #网站证书
     openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:4096 -out server.key
     chmod 400 server.key
-    openssl req -new -sha256 -key server.key -out server.csr
+    echo "==> Attention to:"
+    echo " -> Organization Name"
+    echo " -> Common Name"
+    echo " -> Other Common Name"
+    openssl req -new -sha256 -config $CA_cnf -key server.key -out server.csr
 }
 
 sign_by_CA () {
+    #如果在执行签名命令时，出现“I am unable to access the ./CA/newcerts directory”
+    #修改./CA/openssl.cnf中“dir = ./CA”
+    #然后：
+    [ -d $CA_dir/newcerts ]  || mkdir -p $CA_dir/newcerts
+    [ -f $CA_dir/index.txt ] || > $CA_dir/index.txt
+    [ -f $CA_dir/serial ]    || echo '01' > $CA_dir/serial
+    #再重新签名
+    openssl ca -policy policy_anything -days 1950 -config $CA_cnf\
+        -cert $CA_dir/CA.crt -keyfile $CA_dir/CA.key \
+        -in server.csr -out server.crt
     #policy参数允许签名的CA和网站证书可以有不同的国家、地名等信息
-    if openssl ca -policy policy_anything -days 1950 -cert ca.crt -keyfile ca.key -in server.csr -out server.crt; then
-        echo "==> Done."
-    else
-        #如果在执行签名命令时，出现“I am unable to access the xx/newcerts directory”
-        #修改/etc/ssl/openssl.cnf中“dir = ./CA”
-        #然后：
-        #  mkdir -p CA/newcerts
-        #  touch CA/index.txt
-        #  touch CA/serial
-        #  echo “01″ > CA/serial
-        #再重新签名
-        sudo mkdir -pv /etc/ssl/newcerts
-        sudo touch /etc/ssl/{index.txt,serial}
-        sudo sh -c "echo '01' >/etc/ssl/serial"
-        sudo openssl ca -policy policy_anything -days 1950 -cert ca.crt -keyfile ca.key -in server.csr -out server.crt
-    fi
+    echo "==> Done."
 }
 
-if [ ! -f ca.key -o ! -f ca.crt ]; then
-    echo "==> No ca.key ca.crt. Generating new one ..."
+if [ ! -f $CA_dir/CA.key -o ! -f $CA_dir/CA.crt ]; then
+    echo "==> No CA.key CA.crt. Generating new one ..."
     gen_CA
 fi
 if [ ! -f server.key -o ! -f server.csr ]; then
