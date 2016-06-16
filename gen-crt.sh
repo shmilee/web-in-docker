d63c0ca6e1ea19b5e09e1c39940cab22a0c84378
shmilee <shmilee.zju@gmail.com>
2016-06-16 21:20:49 +0800

    gen sub CA as bonus

--- gen-crt.sh
+++ gen-crt.sh
@@ -2,11 +2,28 @@
 
 CA_dir=./CA
 CA_cnf=$CA_dir/openssl.cnf
+bonus=${1:-subCA}
 
 gen_CA () {
     #CA 证书请求生成及签名
     openssl req -new -x509 -days 3650 -config $CA_cnf -keyout $CA_dir/CA.key -out $CA_dir/CA.crt
     rm -rf $CA_dir/newcerts $CA_dir/index.* $CA_dir/serial*
+    mkdir -p $CA_dir/newcerts
+    > $CA_dir/index.txt
+    echo '01' > $CA_dir/serial
+}
+
+gen_subCA () {
+    #sub CA key + req, 无需 pass phrase
+    openssl genrsa -out ./$bonus.key #-des3
+    openssl req -new -sha256 -config $CA_cnf -key ./$bonus.key -out ./$bonus.csr
+}
+sign_subCA () {
+    openssl ca -policy policy_anything -days 1950 -config $CA_cnf\
+        -cert $CA_dir/CA.crt -keyfile $CA_dir/CA.key -extensions v3_ca\
+        -in ./$bonus.csr -out ./$bonus.crt
+    cat ./$bonus.crt ./$bonus.key > ./${bonus}-all.crt
+    #openssl rsa -in ./$bonus.key >> ./${bonus}-all.crt
 }
 
 gen_server () {
@@ -24,9 +41,9 @@ sign_by_CA () {
     #如果在执行签名命令时，出现“I am unable to access the ./CA/newcerts directory”
     #修改./CA/openssl.cnf中“dir = ./CA”
     #然后：
-    [ -d $CA_dir/newcerts ]  || mkdir -p $CA_dir/newcerts
-    [ -f $CA_dir/index.txt ] || > $CA_dir/index.txt
-    [ -f $CA_dir/serial ]    || echo '01' > $CA_dir/serial
+    #[ -d $CA_dir/newcerts ]  || mkdir -p $CA_dir/newcerts
+    #[ -f $CA_dir/index.txt ] || > $CA_dir/index.txt
+    #[ -f $CA_dir/serial ]    || echo '01' > $CA_dir/serial
     #再重新签名
     openssl ca -policy policy_anything -days 1950 -config $CA_cnf\
         -cert $CA_dir/CA.crt -keyfile $CA_dir/CA.key \
@@ -39,9 +56,17 @@ if [ ! -f $CA_dir/CA.key -o ! -f $CA_dir/CA.crt ]; then
     echo "==> No CA.key CA.crt. Generating new one ..."
     gen_CA
 fi
-if [ ! -f server.key -o ! -f server.csr ]; then
+
+if [ $bonus != subCA ]; then
+    echo "==> Generating Subordinate CA $bonus ..."
+    gen_subCA
+    echo "==> Sign ./$bonus.csr with Root CA ..."
+    sign_subCA
+fi
+
+if [ ! -f server.key -o ! -f server.csr -o ! -f server.crt ]; then
     echo "==> Generating server.key server.csr ..."
     gen_server
+    echo "==> Sign server.csr with CA ..."
+    sign_by_CA
 fi
-echo "==> Sign server.csr with CA ..."
-sign_by_CA
