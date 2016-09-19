6506c0f1aa15ba4d874587d8ee9051319077bfe7
shmilee <shmilee.zju@gmail.com>
2016-09-19 21:47:44 +0800

    change uploadify into jQuery-File-Upload
    * support Mobile browsers
    * abandon flash
    * jQuery, Bootstrap

---
+++ root_files/upload/jQuery-File-Upload/main.js
@@ -0,0 +1,46 @@
+/*
+ * jQuery File Upload Plugin JS Example
+ * https://github.com/blueimp/jQuery-File-Upload
+ *
+ * Copyright 2010, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/MIT
+ */
+
+/* global $, window */
+
+$(function () {
+    'use strict';
+
+    // Initialize the jQuery File Upload widget:
+    $('#fileupload').fileupload({
+        // Uncomment the following to send cross-domain cookies:
+        //xhrFields: {withCredentials: true},
+        url: '/upload/jQuery-File-Upload/main.php',
+        // Enable image resizing, except for Android and Opera,
+        // which actually support image resizing, but fail to
+        // send Blob objects via XHR requests:
+        disableImageResize: /Android(?!.*Chrome)|Opera/
+            .test(window.navigator.userAgent),
+        maxFileSize: 30000000,
+        acceptFileTypes: /(\.|\/)(gif|jpe?g|png|rar|zip|gz|xz|mp3|txt|pdf|m|md|rst|log)$/i
+    });
+
+    // Load existing files:
+    $('#fileupload').addClass('fileupload-processing');
+    $.ajax({
+        // Uncomment the following to send cross-domain cookies:
+        //xhrFields: {withCredentials: true},
+        url: $('#fileupload').fileupload('option', 'url'),
+        dataType: 'json',
+        context: $('#fileupload')[0]
+    }).always(function () {
+        $(this).removeClass('fileupload-processing');
+    }).done(function (result) {
+        $(this).fileupload('option', 'done')
+            .call(this, $.Event('done'), {result: result});
+    });
+
+});
