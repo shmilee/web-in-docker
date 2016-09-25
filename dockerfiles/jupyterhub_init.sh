2d840a01d3b8bb23cb45307d24c9f44ac1a1db96
shmilee <shmilee.zju@gmail.com>
2016-09-26 00:17:17 +0800

    jupyterhub: add iruby kernel + matlab kernel

--- dockerfiles/jupyterhub_init.sh
+++ dockerfiles/jupyterhub_init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sleep ${1:-5}
 config=${2:-/srv/jupyterhub/jupyterhub_config.py}
-hub_options=${@:3}
+hook_script="$3"
+hub_options=${@:4}
 if [ ! -f $config ]; then
     echo "!!! lost jupyterhub_config.py"
     exit 1
@@ -14,9 +15,18 @@ while read line; do
     _USER=$(echo $line | sed "s/.*$_UID:\(.*\):.*<.*>/\1/")
     _PSWD=$(echo $line | sed 's/.*:.*:.*<\(.*\)>.*$/\1/')
     useradd -g users -m -b /srv/jupyterhub -u $_UID -p "$_PSWD" $_USER
+    hash iruby &>/dev/null && {
+        echo "==> Register IRuby kernel for USER: $_USER"
+        su $_USER -c 'iruby register --force'
+    }
 done <<EOF
 $(sed -n '/# PASSWORD OF USER / p' $config)
 EOF
+
+# hook script
+if [ -x "$hook_script" ]; then
+    "$hook_script"
+fi
 
 # run hub
 exec jupyterhub --config=$config $hub_options
