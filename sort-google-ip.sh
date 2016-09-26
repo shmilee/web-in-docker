5ef7340e0221724631c61cd4fc63c04d450b50ad
shmilee <shmilee.zju@gmail.com>
2016-09-26 14:49:27 +0800

    ping google, count, sort

--- sort-google-ip.sh
+++ sort-google-ip.sh
@@ -1,44 +1,41 @@
 #!/bin/bash
-count=$(echo "$@" | grep '|' -o | wc -l)
-((count++))
+## sh ./sort-google-ip.sh $(<./iplist.source)
+
+count=$(echo "$@" | sed 's/[| ]/\n/g' | sed '/^$/d' | sort | uniq | wc -l)
 i=1
-for ip in `echo "$@" | sed 's/|/ /g'`; do
+> 1-ip.list
+for ip in `echo "$@" | sed 's/[| ]/\n/g' | sort | uniq`; do
     echo "##### $((i++)) / $count #####"
-    ping -c 2 $ip && echo $ip >> ip.list
+    ping -c 2 -W 1 $ip && echo $ip >> 1-ip.list
 done
 
-count=$(sort ip.list | uniq | wc -l)
+count=$(sort 1-ip.list | uniq | wc -l)
 i=1
-for ip in `sort ip.list | uniq`; do
+> 2-ip.test
+for ip in `sort 1-ip.list | uniq`; do
     echo "##### $((i++)) / $count #####"
-    (echo -n $ip": "
-    ping -c 5 $ip | awk '/rtt/{print $4}
-/transmitted/ {printf $6 }' | sed 's/%/% /;s/\// /g') >> ip.test
+    (echo -n $ip" "
+    ping -c 10 $ip | awk '/rtt/{print $4}
+/transmitted/ {printf $6 }' | sed 's/%/% /;s/\// /g') >> 2-ip.test
 done
 
-sed -i 's/[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*: 100% //g' ip.test
+sed -i 's/[0-9]*\.[0-9]*\.[0-9]*\.[0-9]* 100% //g' 2-ip.test
 
-## 0% packet loss
-echo "-----00%-----" > ip.result
-cat ip.test | awk '{ if($2=="0%"){print $0}}' | sort -k 4 -n >> ip.result
-cat ip.test | awk '{ if($2=="0%"){print $0}}' | sort -k 4 -n > ip.best.tmp
-## 20% packet loss
-echo "-----20%-----" >> ip.result
-cat ip.test | awk '{ if($2=="20%"){print $0}}' | sort -k 4 -n >> ip.result
-
-## 40% packet loss
-echo "-----40%-----" >> ip.result
-cat ip.test | awk '{ if($2=="40%"){print $0}}' | sort -k 4 -n >> ip.result
-
-## 60% packet loss
-echo "-----60%-----" >> ip.result
-cat ip.test | awk '{ if($2=="60%"){print $0}}' | sort -k 4 -n >> ip.result
-
-## 80% packet loss
-echo "-----80%-----" >> ip.result
-cat ip.test | awk '{ if($2=="80%"){print $0}}' | sort -k 4 -n >> ip.result
+## sort test result
+cat > 3-ip.result.md <<EOF
+|     IP     | packet loss | RTT min | RTT avg | RTT max | RTT mdev |
+|:----------:|:-----------:|:-------:|:-------:|:-------:|:--------:|
+EOF
+sort -k 4,4 -k 2,2 -k 6,6 -n 2-ip.test \
+    | sed -e 's/ /|/g' -e 's/^/|/' -e 's/$/|/' >> 3-ip.result.md
+#awk '{print "|"$1"|"$2"|"$3"|"$4"|"$5"|"$6"|"}'
 
 ## BEST output
-awk 'BEGIN{"wc -l ip.best.tmp" | getline; weight=$1}
-{ if($2=="0%") {print "server "$1"443 weight="weight";"; weight--;} }' ip.best.tmp > ip.best
-rm ip.best.tmp
+sort -k 4,4 -k 2,2 -k 6,6 -n 2-ip.test \
+    | awk '{ if($2=="0%") {printf $1"|"}} END {printf $1}' > 4-ip.best
+
+## nginx conf output
+sed 's/[| ]/\n/g' 4-ip.best > 4-ip.best.tmp
+awk 'BEGIN{"wc -l 4-ip.best.tmp" | getline; weight=$1; weight++; print "fair;";}
+{print "server "$1":443 weight="weight";"; weight--;}' 4-ip.best.tmp > 4-ip.best.conf
+rm 4-ip.best.tmp
