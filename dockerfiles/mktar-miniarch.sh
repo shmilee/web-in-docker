9550b94b2ac10b6b0c7074ade16462e735f3b740
shmilee <shmilee.zju@gmail.com>
2016-11-24 19:06:25 +0800

    slim docker images

--- dockerfiles/mktar-miniarch.sh
+++ dockerfiles/mktar-miniarch.sh
@@ -32,7 +32,9 @@ PKGIGNORE=(
     dhcpcd
     iproute2
     jfsutils
+    licenses
     linux
+    logrotate
     lvm2
     man-db
     man-pages
@@ -82,17 +84,30 @@ touch $ROOTFS/root/.gnupg/dirmngr_ldapservers.conf
 arch-chroot $ROOTFS /bin/sh -c 'pacman-key -r E2054497 && pacman-key --lsign-key E2054497; pkill dirmngr; pkill gpg-agent'
 cat ./mkimage-arch-pacman.conf > $ROOTFS/etc/pacman.conf
 
-arch-chroot $ROOTFS /bin/sh -c "ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime"
-echo 'en_US.UTF-8 UTF-8' > $ROOTFS/etc/locale.gen
-echo 'zh_CN.UTF-8 UTF-8' >> $ROOTFS/etc/locale.gen
-arch-chroot $ROOTFS locale-gen
-echo 'LANG="en_US.UTF-8"' > $ROOTFS/etc/locale.conf
-#arch-chroot $ROOTFS /bin/sh -c 'echo "Server = http://mirrors.ustc.edu.cn/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist'
+# mirror
 sed -n '/^#/!p' /etc/pacman.d/mirrorlist | sed '/^$/d' > $ROOTFS/etc/pacman.d/mirrorlist
 
+# zoneinfo
+arch-chroot $ROOTFS /bin/sh -c "ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime"
+find $ROOTFS/usr/share/zoneinfo/ -type f -not \( -name 'UTC' -o -name 'Shanghai' \) -exec rm {} \;
+
+# locale
+echo 'en_US.UTF-8 UTF-8' > $ROOTFS/etc/locale.gen
+#echo 'zh_CN.UTF-8 UTF-8' >> $ROOTFS/etc/locale.gen
 # remove locale information
-find $ROOTFS/usr/share/locale -mindepth 1 -maxdepth 1 -type d -not \( -name 'en_US' -o -name 'zh_CN' \) -exec rm -r {} \;
+find $ROOTFS/usr/lib/gconv/ -type f -not \( -name 'UNICODE.so' -o -name 'UTF-*.so' \) -exec rm {} \;
+find $ROOTFS/usr/share/i18n/locales/ -type f -not \( -name 'POSIX' -o -name 'i18n' \
+    -o -name 'iso14651_*' -o -name 'translit_*' \
+    -o -name 'en_US' -o -name 'en_GB' -o -name 'zh_CN' \) -exec rm {} \;
+find $ROOTFS/usr/share/i18n/charmaps/ -type f -not \( -name 'UTF-8.gz' \) -exec rm {} \;
+find $ROOTFS/usr/share/locale -mindepth 1 -maxdepth 1 -type d -not \( -name 'en_US' \) -exec rm -r {} \;
 touch $ROOTFS/usr/share/locale/locale.alias
+arch-chroot $ROOTFS locale-gen
+echo 'LANG="en_US.UTF-8"' > $ROOTFS/etc/locale.conf
+
+# terminfo
+find $ROOTFS/usr/share/terminfo/ -type f -not \( -name 'ansi' -o -name 'cygwin' -o -name 'linux' \
+    -o -name 'vt100' -o -name 'vt220' -o -name 'xterm' \) -exec rm {} \;
 
 # remove libgo.so
 rm -rf $ROOTFS/usr/lib/libgo.so*
