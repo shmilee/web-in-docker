71eb10327652c11e1f236dbf2004af2c9dbe5c32
shmilee <shmilee.zju@gmail.com>
2019-07-02 19:46:38 +0800

    deploy nginx_v2ray in VPS

--- readme.md
+++ readme.md
@@ -269,8 +269,8 @@ generate `dhparam.pem` `server-v2ray.{crt,key}` for nginx
 
 ```
 cd ./v2ray
-bash ./deploy.sh ip-addr [port] [domain] [v2raypath]
+bash ./deploy.sh ip-addr [port] [domain]
 ls v2ray-deploy/
 ```
 
-* download client config `v2ray-deploy/v2ray-client.config`
+* download client config `v2ray-deploy/v2ray-client-config.json`
--- v2ray/deploy.sh
+++ v2ray/deploy.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
 ipaddr=${1}
-port=${2:-12345}
+port=${2:-$RANDOM}
 domain=${3:-$ipaddr}
-v2raypath=${4:-v2rayv2ray}
+v2raypath=$(mktemp -p download/huge -t 'dataid=XXXXXXXXXX' -u)
 
 if [ -z $ipaddr ];then
     cat <<EOF
-usage: $0 ip-addr [port] [domain] [v2raypath]
+usage: $0 ip-addr [port] [domain]
 default:
-  port       12345
+  port       \$RANDOM
   domain     ip-addr
-  v2raypath  'v2rayv2ray'
 EOF
     exit 1
 fi
 
 [ -d v2ray-deploy ] && rm -r v2ray-deploy/
-mkdir -pv v2ray-deploy/etc
 
 echo "==> copy etc files ..."
+mkdir -pv v2ray-deploy/etc/ssl-certs
 cp -r modules-enabled sites-enabled monitrc ../etc/nginx.conf v2ray-deploy/etc/
 chmod 600 v2ray-deploy/etc/monitrc
 #sudo chown -v 0:0 v2ray-deploy/etc/monitrc
 sed -i -e "s|{{domain-name}}|$domain|" -e "s|{{v2raypath}}|$v2raypath|" v2ray-deploy/etc/sites-enabled/nginx-*.vhost
-mkdir v2ray-deploy/etc/ssl-certs
 
 echo "==> log dir ..."
 mkdir v2ray-deploy/log
@@ -34,32 +32,27 @@ uuid1=$(uuidgen)
 uuid2=$(uuidgen)
 #uuid1='x-x-x-x-x' # test
 #uuid2='x-x-x-x-x'
-cp v2ray-server.config v2ray-client.config v2ray-deploy/
-sed -i -e "s|{{UUID1}}|${uuid1}|" -e "s|{{UUID2}}|${uuid2}|" \
+sed -e "s|{{UUID1}}|${uuid1}|" -e "s|{{UUID2}}|${uuid2}|" \
     -e "s|{{port1}}|$port|" \
     -e "s|{{v2raypath}}|$v2raypath|" \
-    v2ray-deploy/v2ray-server.config
-sed -i -e "s|{{UUID1}}|${uuid1}|" -e "s|{{UUID2}}|${uuid2}|" \
+    v2ray-server-config.json > v2ray-deploy/v2ray-server-config.json
+sed -e "s|{{UUID1}}|${uuid1}|" -e "s|{{UUID2}}|${uuid2}|" \
     -e "s|{{ip-addr}}|$ipaddr|" -e "s|{{port1}}|$port|" \
     -e "s|{{domain-name}}|$domain|" -e "s|{{v2raypath}}|$v2raypath|" \
-    v2ray-deploy/v2ray-client.config
+    v2ray-client-config.json > v2ray-deploy/v2ray-client-config.json
 
 
 echo "==> Done."
 cat <<'EOF' | sed "s|{{port1}}|$port|g" 
 
-1. Run v2ray
-docker run --rm -d --name v2ray -p {{port1}}:{{port1}} \
-    -v $PWD/v2ray-deploy/v2ray-server.config:/etc/v2ray/config.json \
-    -v $PWD/v2ray-deploy/log:/var/log/v2ray:rw \
-    v2ray/official
-
-2. Put dhparam.pem server-v2ray.{crt,key} in v2ray-deploy/etc/ssl-certs
-   Run nginx
-docker run --rm -d --name nginx --link v2ray -p 80:80 -p 443:443 \
+1. Put dhparam.pem server-v2ray.{crt,key} in v2ray-deploy/etc/ssl-certs
+   Run nginx and v2ray
+docker run --rm --name nginx_v2ray \
+    -p 80:80 -p 443:443 -p {{port1}}:{{port1}} \
     -v $PWD/v2ray-deploy/etc:/srv/etc:ro \
+    -v $PWD/v2ray-deploy/v2ray-server-config.json:/etc/v2ray/config.json:ro \
     -v $PWD/v2ray-deploy/log:/srv/log:rw \
-    shmilee/lnmp:using
+    shmilee/v2ray
 
-3. If containers run scucessfully, the option '--rm' can replace by '--restart=always'
+2. If containers run scucessfully, the option '--rm' can be replaced by '--detach --restart=always'
 EOF
