abce990185939399866278505a7cafe05d05d2cf
shmilee <shmilee.zju@gmail.com>
2021-12-11 12:54:46 +0800

    index.html: script to body

--- root_files/js/bg.js
+++ root_files/js/bg.js
@@ -12,4 +12,30 @@ $(document).ready(function() {
       $("body").css("background-size",'100% auto');
     }
   });
+
+//  var a_idx = 0;
+//  $("body").click(function(e) {
+//    var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善");
+//    var $i = $("<span/>").text(a[a_idx]);
+//    a_idx = (a_idx + 1) % a.length;
+//    var x = e.pageX,
+//    y = e.pageY;
+//    $i.css({
+//      "z-index": 99,
+//      "top": y - 25,
+//      "left": x,
+//      "position": "absolute",
+//      "font-weight": "bold",
+//      "color": "#ff6651"
+//    });
+//    $("body").append($i);
+//    $i.animate({
+//        "top": y - 200,
+//        "opacity": 0.5
+//      },
+//      1500,
+//      function() {
+//        $i.remove();
+//      });
+//  });
 });
